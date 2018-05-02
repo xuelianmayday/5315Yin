98078ff0debf404527684ed34b6ba781b7c8ba5c
chloe <yinw1@seattleu.edu>
2018-05-01 23:37:40 -0700

    Remove redundant comma

--- empdata.js
+++ empdata.js
@@ -4,7 +4,7 @@
       "name": "Kelly Yin",
       "email": "kyin@gmail.com",
       "phone": "918 223 9819",
-      "city": "Seattle"
+      "city": "Seattle",
       "zipcode": "98105"
     },
     {
@@ -12,35 +12,35 @@
       "email": "bspears.gmail.com",
       "phone": "347 753 8712",
       "city": "Los Angeles",
-      "zipcode": "53701",
+      "zipcode": "53701"
     },
     {
       "name": "Will Hood",
       "email": "whood@gmail.com",
       "phone": "917 652 3361",
       "city": "San Diego",
-      "zipcode": "45071",
+      "zipcode": "45071"
     },
     {
       "name": "Merissa Wang",
       "email": "mwang@gmail.com",
       "phone": "917 823 2819",
       "city": "San Francisco",
-      "zipcode": "47910",
+      "zipcode": "47910"
     },
     {
       "name": "Robin Yang",
       "email": "ryang@gmail.com",
       "phone": "301 257 6591",
       "city": "Chicago",
-      "zipcode": "37452",
+      "zipcode": "37452"
     },
     {
       "name": "Jenny Hurst",
       "email": "jhurst@gmail.com",
       "phone": "101 801 9981",
       "city": "Dallas",
-      "zipcode": "67081",
+      "zipcode": "67081"
     }
   ]
 }
